0565e7d0fa87b5f93366ffc44c6f43648e15d0fc
Rondi Wellum <rondi.wellum@lighthouselabservices.com>
2021-12-29 14:41:05 -0500

    testing r cod

--- convert/functions.R
+++ convert/functions.R
@@ -25,7 +25,7 @@ ConvertFields <- function(dt_in = NULL, conf = NULL) {
 
     # field not found
     if (is.null(res_dt[[fld_id]])) {
-       res_dt[[fld_id]] <- "?"
+       res_dt[[fld_id]] <- "???"
       next
       }
 

4bf34eadeb8c8de628e6d0d2aa396df5f2d9a93f
Vitali <vitali@khvatkov.com>
2022-01-04 12:53:27 -0600

    config update

--- convert/postprocessing_functions.R
+++ convert/postprocessing_functions.R
@@ -2,11 +2,17 @@
 
 Leads <- function(res_dt, conf_opt = NULL){
   
+  # check expected fields present
+  expected_fields = c(   "companyname" , "Individual" , "first Name" ,  "Last Name"  )
+  if( ! all(expected_fields %in% names(res_dt)) ) return(res_dt)
+  
   # prefer company lead
   res_dt[companyname != '', Individual := 'N']
   
   # fill individual last name and first name
   name_replace = conf_opt$nameDefault
+  if(is.null(name_replace)) return(res_dt)
+    
   res_dt[Individual == "Y" & `Last Name` == '', `Last Name` := name_replace ]
   res_dt[Individual == "Y" & `first Name` == '', `first Name` := name_replace ]
   

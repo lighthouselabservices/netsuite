608569e21045f48417668ab67c98108bbeadc794
Vitali <vitali@khvatkov.com>
2022-01-04 12:21:23 -0600

    post processing update

--- convert/postprocessing_functions.R
+++ convert/postprocessing_functions.R
@@ -5,8 +5,10 @@ Leads <- function(res_dt, conf_opt = NULL){
   # prefer company lead
   res_dt[companyname != '', Individual := 'N']
   
-  # fill individual last name
-  res_dt[Individual == "Y" & `Last Name` == '', `Last Name` := conf_opt$ ]
+  # fill individual last name and first name
+  name_replace = conf_opt$nameDefault
+  res_dt[Individual == "Y" & `Last Name` == '', `Last Name` := name_replace ]
+  res_dt[Individual == "Y" & `first Name` == '', `first Name` := name_replace ]
   
   return(res_dt)
 }

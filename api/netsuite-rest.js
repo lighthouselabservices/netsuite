c1b35c92ebc65d947f6a07a715984a6e93ba6f8b
Tabi <86167237+Cooleyt@users.noreply.github.com>
2022-01-19 16:57:06 -0500

    Add files via upload

---
+++ api/netsuite-rest.js
@@ -0,0 +1,80 @@
+const OAuth = require('oauth-1.0a')
+const crypto = require('crypto')
+var got = require('got');
+
+class NetsuiteRest {
+    constructor(options) {
+        this.consumer_key = options.consumer_key;
+        this.consumer_secret_key = options.consumer_secret_key;
+        this.token = options.token;
+        this.token_secret = options.token_secret;
+        this.version = '1.0';
+        this.algorithm = 'HMAC-SHA256';
+        this.realm = options.realm;
+        this.base_url = options.base_url;
+    }
+    getAuthorizationHeader(options) {
+        const oauth = OAuth({
+            consumer: {
+                key: this.consumer_key,
+                secret: this.consumer_secret_key,
+            },
+            realm: this.realm,
+            signature_method: this.algorithm,
+            hash_function(base_string, key) {
+                return crypto
+                    .createHmac('sha256', key)
+                    .update(base_string)
+                    .digest('base64')
+            },
+        })
+        return oauth.toHeader(
+            oauth.authorize({
+                url: options.url,
+                method: options.method
+            }, {
+                key: this.token,
+                secret: this.token_secret,
+            })
+        )
+    }
+    request(opts) {
+        const {
+            path = '*',
+            method = 'POST',
+            body = '',
+            heads={}
+        } = opts;
+
+        let uri = `https://${this.realm.replace("_SB",'-sb')}.suitetalk.api.netsuite.com/services/rest/${path}`;
+
+        if (this.base_url)
+            uri = `${this.base_url}/services/rest/${path}`;
+
+        const options = {
+            url: uri,
+            method,
+            throwHttpErrors: true,
+            decompress: true,
+            hooks: {
+                afterResponse: [(response) => {
+                    return {
+                        ...response,
+                        'data': response.body ? JSON.parse(response.body) : null,
+                    };
+                }
+                ]
+            }
+        };
+        options.headers = this.getAuthorizationHeader(options);
+        if(heads){
+            options.headers ={...options.headers,...heads}
+        }
+        if (body) {
+            options.body = body;
+            options.headers.prefer = "transient";
+        }
+        return got(options);
+    }
+}
+module.exports = NetsuiteRest;

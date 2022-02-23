fec68411f8623c8db72ebfbb6bad1f9d0d5680b0
Vitali <vitali@khvatkov.com>
2022-02-22 23:39:30 -0600

    PubSub code updates

--- cloudfunc/test.js
+++ cloudfunc/test.js
@@ -4,24 +4,24 @@ const api = require('./plugins/netsuiteApi.js');
 
 // sample lead
 const lead = {
-  firstName: 'Vitali',
-  lastName: 'Test',
-  companyName: `VK viaAPI1 ${Date().toString().substring(0, 25)} `,
-  email: 'apitest1@company.com',
-  entityStatus: '6',
-  isPerson: true,
-  leadSource: '-6',
-  subsidiary: '2',
+	firstName:'FirstNameTest',
+	lastName:'LastNameTest',
+	companyName: `VkTest from CloudFunc ${Date().toString().substring(0, 25)} `,
+	email: 'test@cloudfunc.com',
+	entityStatus: '6',
+	isPerson: true,
+	leadSource: '-6',
+	subsidiary: '2',
 };
 
 // create lead in Netsuite
 api
-  .createCustomer(lead)
-  .then((resp) =>
-    console.log(
-      resp.statusCode === 204 ?
-        'LEAD CREATED: ' + lead.companyName :
-        'ERROR CODE:' + resp.statusCode,
-    ),
-  )
-  .catch((e) => console.log(e));
+	.createCustomer(lead)
+	.then((resp) =>
+		console.log(
+			resp.statusCode === 204 ?
+				'LEAD CREATED: ' + lead.companyName :
+				'ERROR CODE:' + resp.statusCode,
+		),
+	)
+	.catch((e) => console.log(e));
